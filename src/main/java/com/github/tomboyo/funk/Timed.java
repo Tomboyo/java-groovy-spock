2d9dfb125396002d20a54a934e06aa012edd00a3
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2019-11-21 00:57:45 -0500

    Timed - Timed holds duration of computation

--- src/main/java/com/github/tomboyo/funk/Timed.java
+++ src/main/java/com/github/tomboyo/funk/Timed.java
@@ -1,23 +1,36 @@
 package com.github.tomboyo.funk;
 
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.function.Supplier;
 
 public final class Timed<Result> {
 
   private final Result result;
+  private final Duration duration;
 
   private Timed(
-    Result result
+    Result result,
+    Duration duration
   ) {
     this.result = result;
+    this.duration = duration;
   }
 
   public Result getResult() {
     return result;
   }
 
-  public static <R> Timed<R> time(Supplier<R> unitOfWork) {
-    return new Timed<>(unitOfWork.get());
+  public Duration getDuration() {
+    return duration;
+  }
+
+  public static <R> Timed<R> time(Supplier<R> unitOfWork, Clock clock) {
+    Instant start = clock.instant();
+    R result = unitOfWork.get();
+    Instant end = clock.instant();
+    return new Timed<>(result, Duration.between(start, end));
   }
 
 }
--- src/test/groovy/com/github/tomboyo/funk/TimedSpec.groovy
+++ src/test/groovy/com/github/tomboyo/funk/TimedSpec.groovy
@@ -1,6 +1,8 @@
 import spock.lang.Specification
 
+import java.time.Clock
 import java.time.Duration
+import java.time.Instant
 
 import com.github.tomboyo.funk.Timed
 
@@ -9,11 +11,28 @@ class TimedSpec extends Specification {
   def "timed functions return their result"() {
     given:
     def unitOfWork = { -> 'result of computation' }
+    def clock = Clock.systemDefaultZone()
 
     when:
-    def result = Timed.time(unitOfWork).getResult()
+    def result = Timed.time(unitOfWork, clock).getResult()
 
     then:
     result == 'result of computation'
+  }
+
+  def "timed functions return their run duration"() {
+    given:
+    def unitOfWork = { -> 'result of computation' }
+    def clock = Mock(Clock)
+
+    def start = Instant.ofEpochMilli(0)
+    def expectedDuration = Duration.ofSeconds(1)
+    clock.instant() >>> [ start, start.plus(expectedDuration) ]
+
+    when:
+    def actualDuration = Timed.time(unitOfWork, clock).getDuration()
+
+    then:
+    actualDuration == expectedDuration
   }
 }
